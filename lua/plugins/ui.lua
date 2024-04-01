9f6c008575055af7b27b5ea2a5327e52c2990016
Slava Maksimov <hello@slavamak.com>
2024-04-02 02:30:17 +0300

    feat: exclude search files in .git

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -66,7 +66,7 @@ return {
         },
         pickers = {
           find_files = {
-            hidden = true
+            find_command = { 'rg', '--files', '--hidden', '--glob', '!**/.git/*' },
           },
           git_files = {
             show_untracked = true
