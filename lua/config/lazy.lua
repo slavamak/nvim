66f4a6623ea6c0418060339f33c37978709a9471
Slava Maksimov <hello@slavamak.com>
2024-04-01 19:03:30 +0300

    feat: disable change detection

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,24 +1,27 @@
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
+  vim.fn.system {
     'git',
     'clone',
     '--filter=blob:none',
     'https://github.com/folke/lazy.nvim.git',
     '--branch=stable',
     lazypath,
-  })
+  }
 end
 
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup('plugins', {
+  change_detection = {
+    enabled = false,
+  },
   defaults = {
     lazy = true,
   },
   install = {
     missing = true,
     colorscheme = { 'kanagawa-wave', 'github_light_default' },
-  }
+  },
 })
