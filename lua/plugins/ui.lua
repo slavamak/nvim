5464b0e683151bf0f050779495b0365d0aa928e1
Slava Maksimov <hello@slavamak.com>
2024-05-12 13:56:00 +0300

    revert: change notification messages position
    commit 78d2772

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -99,16 +99,7 @@ return {
 
   {
     'j-hui/fidget.nvim',
-    opts = {
-      notification = {
-        view = {
-          stack_upwards = false,
-        },
-        window = {
-          align = 'top',
-        },
-      },
-    },
+    config = true,
   },
 
   {
