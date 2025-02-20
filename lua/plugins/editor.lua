6bfce924a87dda2d568b3bc851f4cb39f66dca46
Slava Maksimov <hello@slavamak.com>
2025-02-20 22:16:32 +0300

    feat: add some snacks plugins

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -112,9 +112,14 @@ return {
 
   {
     'folke/snacks.nvim',
+    priority = 1000,
+    lazy = false,
     ---@type snacks.Config
     opts = {
       bigfile = {},
+      image = {},
+      lazygit = {},
+      notifier = {},
     },
   },
 }
